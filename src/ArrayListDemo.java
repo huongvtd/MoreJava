8af6a2baa471d030e1b4ad89cad2724a255d1ab8
huong <huongvtd@gmail.com>
2023-04-16 22:13:09 +0700

    041620233

--- src/ArrayListDemo.java
+++ src/ArrayListDemo.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
-        ArrayList <String> nameList = new ArrayList<> ();
+        ArrayList<String> nameList = new ArrayList<>();
 
         nameList.add(0, "Shi");
         System.out.println(nameList);
@@ -13,7 +14,7 @@ public class ArrayListDemo {
 
         System.out.println(nameList.get(1));
 
-        nameList.set(1,"Bim");
+        nameList.set(1, "Bim");
         System.out.println(nameList);
 
         String removed = nameList.remove(1);
@@ -27,5 +28,11 @@ public class ArrayListDemo {
         for (String value : nameList) {
             System.out.println(value);
         }
+        System.out.println("-------------------------------");
+        Iterator<String> iterator = nameList.iterator();
+        String name = iterator.next();
+        System.out.println("1st name is "+ name);
+        name = iterator.next();
+        System.out.println("2nd name is "+ name);
     }
 }
--- src/HashMapDemo.java
+++ src/HashMapDemo.java
@@ -2,9 +2,22 @@ import java.util.HashMap;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-        HashMap<String, Integer> students = new HashMap<String, Integer>();
-        students.put("Bo", 5);
-        students.put("Bom", 6);
+        HashMap<String, Integer> students = new HashMap<>();
+        students.put("Bong", 5);
+        students.put("Bo", 6);
+        students.put("Bi", 5);
+        System.out.println(students);
+
+        students.clone();
+        System.out.println(students);
+
+        System.out.println("Bong is "+ students.get("Bong") + " years old.");
+
+        System.out.println("All keys are " + students.keySet());
+        System.out.println("All values are " + students.values());
+        System.out.println("All pairs are " + students.entrySet());
+
+        students.replace("Bi",8 );
         System.out.println(students);
     }
 }
